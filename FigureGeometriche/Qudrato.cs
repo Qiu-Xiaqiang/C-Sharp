698776c56d1031efc665122985e9e731d3c4c8f7
39331 <39331@>
2024-03-25 19:38:10 +0100

    Aggiungere i file di progetto.

---
+++ FigureGeometriche/Qudrato.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureGeometriche
+{
+    internal class Qudrato : Figure
+    {
+        double _lato;
+        public Qudrato(double Base )
+        {
+            _lato= Base;
+        }
+        public double Base
+        {
+            get { return _lato; }
+            set { _lato = value; }
+        }
+        public double Altezza
+        {
+            get;
+        }
+        public double Perimetro()
+        {
+            return _lato * 4;
+        }
+        public double Area()
+        {
+            return _lato * _lato;
+        }
+        public override string ToString()
+        {
+            return string.Format($"Quadrato:- Lato:{_lato,-10} Area:{Area(),-10} Perimetro:{Perimetro(),-10}");
+        }
+
+    }
+}
