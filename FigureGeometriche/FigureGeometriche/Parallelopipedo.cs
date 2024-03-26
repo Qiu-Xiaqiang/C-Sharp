22395a1a18a2b83fb517b4ebea793d551ff1d3be
Xiaqiang Qiu <122173121+Qiu-Xiaqiang@users.noreply.github.com>
2024-03-26 08:54:33 +0100

    Add files via upload

---
+++ FigureGeometriche/FigureGeometriche/Parallelopipedo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureGeometriche
+{
+    internal class Parallelopipedo : Rettangolo
+    {
+        double _profondità;
+
+        public Parallelopipedo(double Base,double altezza, double profondità) : base(Base,altezza)
+        {
+            _profondità = profondità;
+        }
+        public double Profondita
+        {
+            get { return _profondità; }
+            set { _profondità = value; }
+        }
+        override public double Area()
+        {
+            return _profondità * base.Area();//Metodo con ovveride che è una caratteristica Polimorfica
+            //NB:anche con le classi astratte si usa il metodo ovveride nelle classi ereditate invece la classe astratta si scrive public abstract double + Firma
+        }
+        public override string ToString()
+        {
+            return string.Format($"Parallelopipedo-: Profondità:{_profondità,-10}Area:{Area(),-10}");
+        }
+    }
+}
