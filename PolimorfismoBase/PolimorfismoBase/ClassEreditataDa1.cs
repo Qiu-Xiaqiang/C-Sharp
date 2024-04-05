aeecf08fd6b19115a1a437524242a67aa3cf925a
Xiaqiang Qiu <122173121+Qiu-Xiaqiang@users.noreply.github.com>
2024-04-06 00:59:49 +0200

    Virtual e override con visualizzazione

---
+++ PolimorfismoBase/PolimorfismoBase/ClassEreditataDa1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseAstratta
+{
+    internal class ClassEreditataDa1 : ClasseEredita1
+    {
+        int _cifra;
+        public ClassEreditataDa1(int cifra,int numero,int numero2) : base (numero,numero2)
+        {
+            _cifra = cifra;
+        }
+        public override string Visualizza()
+        {
+            return String.Format($"{base.Visualizza()}Cifra:{_cifra}");
+        }
+    }
+}
