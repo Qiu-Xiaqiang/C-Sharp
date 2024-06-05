03c0d72b31597d628ba387ec99cbe0226479f84f
Xiaqiang Qiu <122173121+Qiu-Xiaqiang@users.noreply.github.com>
2024-06-05 12:21:37 +0200

    Conversione PDF to Word
    File troppo grande quindi caricato in program.cs
    In uno converte un singolo pdf in word, invece in un'altro trova il percorso della cartella contenente tantissimi pdf e lo converte tutte in word

---
+++ PDF to Word/CartellaPdfToWord.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Aspose.Pdf;
+using Aspose.Pdf.Devices;
+using Aspose.Pdf.Text;
+using static Aspose.Pdf.SaveOptions;
+
+namespace PdfTotsxt
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string cartellaPdf = "C:\\Users\\qiupa\\Downloads\\PDFFolder"; // Cartella contenente i file PDF nel percorso del tuo computer
+            string cartellaWord = "C:\\Users\\qiupa\\Downloads\\Word"; // Cartella dove salvare i file Word nel percorso del tuo computer
+            Conversione(cartellaPdf, cartellaWord);
+        }
+        static void Conversione(string cartellaPdf, string cartellaWord)
+        {
+            string[] filePDF = Directory.GetFiles(cartellaPdf, "*.pdf");//Ottengo tutti i file nella cartella iniziale con un array
+            try
+            {
+                if (!Directory.Exists(cartellaWord))
+                {
+                    Directory.CreateDirectory(cartellaWord);//Creo la cartella di word se utente non lo ha creato
+                }
+                foreach (string x in filePDF)//Ciclo foreach per scorrere intera cartella pdf contente i file pdf dove ho dichiarato una variabile x
+                {
+                    try
+                    {
+                        Document documentoPDF = new Document(x);//Creo un'istanza di Document 
+                        DocSaveOptions salvataggio = new DocSaveOptions();
+                        salvataggio.Format = DocSaveOptions.DocFormat.DocX;//Salvo in formato DocX
+                        string nomeFile = Path.GetFileNameWithoutExtension(x);//Funzione per determinare il percorso di output per il file word
+                        string percorsoOutput = Path.Combine(cartellaWord, nomeFile + ".docx");//Funzione combine per dare al nome degli file convertito con lo stesso nome del pdf 
+                        Console.WriteLine($"Convertito con successo big congratulazioni :{x,-10}  {percorsoOutput,-10}");
+                        documentoPDF.Save(percorsoOutput, salvataggio);//Infine salvo il file in word 
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Errore {x}: {e.Message}");//Genera un'eccezione se il compito non andato a buon fine
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Errore:" + e.Message);//Stamperà eccezione se non è andato a buon fine
+            }
+        }
+    }
+}
---
+++ PDF to Word/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Pdf;
+
+namespace pdfToWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string percorsoP = "C:\\Users\\qiupa\\Downloads\\Filepd.pdf";
+            string percorsoW = "C:\\Users\\qiupa\\Downloads\\documento.docx";
+            try
+            {
+                Document documentoPDF = new Document(percorsoP);
+                DocSaveOptions salvataggio = new DocSaveOptions();//Creo un'istanza di DocSave
+                salvataggio.Format = DocSaveOptions.DocFormat.DocX;//Lo imposto come output il formato in DocX
+                Console.WriteLine("Convertito con successo");
+                documentoPDF.Save(percorsoW, salvataggio);//Alla fine lo salvo in word
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Errore" + e.Message);
+            }
+
+        }
+    }
+}
